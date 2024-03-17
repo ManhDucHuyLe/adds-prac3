2c79d4d7e1dd4bae48e3d123d6a3a26700f97120
ManhDucHuyLe <140767036+ManhDucHuyLe@users.noreply.github.com>
2024-03-17 17:22:30 +1030

    2nd Update Reverser.cpp

--- Reverser.cpp
+++ Reverser.cpp
@@ -6,13 +6,12 @@
 Reverser::Reverser() {}
 
 int Reverser::reverseDigit(int value) {
-  if (value < 0 || value > 9) {
-        return -1;
-  }
   string strNum = to_string(value);
   int l = strNum.length();
   if (value < 10) {
     return value;
+  } else if (value < 0 || value > 9) {
+        return -1;
   } else {
     int a = value % 10;
     a *= pow(10, l - 1);
@@ -24,8 +23,10 @@ int Reverser::reverseDigit(int value) {
 string Reverser::reverseString(string characters) {
   int l = characters.length();
   int e = characters.empty();
-  if (l == 1 || e) {
+  if (e) {
     return "ERROR";
+  } else if (l == 1) {
+    return characters;
   } else {
     char a = characters[l - 1];
     string reversedSubstring = reverseString(characters.substr(0, l - 1));
